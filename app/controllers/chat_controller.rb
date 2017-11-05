178d7df0417795704d78f273f0e34afb1303d0b9
Dorje <ku.dorjee@gmail.com>
2017-11-05 09:34:28 +0545

    Create rails app install 'pusher' gem and integrated with app create chat controller and views

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,4 @@
+class ChatController < ApplicationController
+  def hello
+  end
+end
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '426327'
+Pusher.key = 'dd9f497c2ae8fad930b0'
+Pusher.secret = '62e1626b2750349e66a2'
+Pusher.cluster = 'ap2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
